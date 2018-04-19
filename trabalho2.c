d6f69a226dff3caccb31324268a3be72d05bd0e4
Ícaro <icarooliv2@gmail.com>
2018-04-19 15:10:05 -0300

    adds rows and columns count

--- trabalho2.c
+++ trabalho2.c
@@ -4,17 +4,21 @@
 int main () {
     FILE *arq;
     char c, end = '\n';
-    int qtdLinhas = 0, qtdColunas = 0;
+    int qtdLinhas = 0, qtdColunas = 1;
     char* filename = "DataSet/asphalt/asphalt_01.txt";
+    int eol = 0;
 
     if ((arq = fopen(filename, "r")) == NULL) {
         printf("Erro ao abrir o arquivo.");         
     } else {
-        //Lendo o arquivo 1 por 1
         while(fread(&c, sizeof(char), 1, arq)) {
-            if(c == end) {
+            if (c == '\n'){
                 qtdLinhas++;
+                eol = 1;
             }
+            if(c == ';' && eol == 0){
+                qtdColunas++;
+            } 
         }
     }
 
@@ -24,6 +28,5 @@ int main () {
     fclose(arq);
 
 }
-
 
 // DataSet/asphalt/asphalt_01.txt
