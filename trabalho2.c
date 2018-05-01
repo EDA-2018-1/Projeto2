b96a54c211cd387b73f41f7a2356dcb82e39fc5c
Ícaro <icarooliv2@gmail.com>
2018-05-01 20:41:30 -0300

    adds matrix-file related functions and ilbp (not working)

--- trabalho2.c
+++ trabalho2.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
-#include<string.h>  
-#include<unistd.h> 
+#include <time.h>
+#include <string.h>  
+#include <unistd.h> 
+#include <math.h>
 
-void foo(int *buf, int count) {
-    for(int i = 0; i < count; ++i)
-        buf[i] = i;
-}
-
-/* sorteiaTesteTreino
-    params: 
-null
-    return: 
-retorna um array de 50 posições unicas dos 50 arquivos que serão de teste ou leitura
-*/
 int *sorteiaTesteTreino(){
     srand(time(NULL));
     int *n = (int *)malloc(50*sizeof(int)); 
@@ -35,48 +25,42 @@ int *sorteiaTesteTreino(){
             ++y;
         }
         ++x;
-    }while(x < 50);
-
+    } while(x < 50);
     return n;
 }
 
-/* salvaArquivos
-    params: 
-int imagemTreino = array para numeros dos arquivos de teste 
-int imagemTeste = array para numeros dos arquivos de treino
-int testeOuTreino: 0 teste 1 treino
-int gramaOuAsfalto: 0 grama 1 asphalt
-*/
-
-FILE* salvaArquivos(int *array, int testeOuTreino, int gramaOuAsfalto){
+int salvaArquivos(int *array, int testeOuTreino, int gramaOuAsfalto){
+    char nome[20];
     char diretorio[256];
     char linha[256];
-    char nome[10];
     int i = 0;
-    
-    for (i=0; i<25; i++){
-        printf("%d\n", array[i]);
-    }
+    int tipo;
 
     if(testeOuTreino == 0 && gramaOuAsfalto == 0){
         strcpy(nome,"teste_grass.txt");
         strcpy(diretorio,"DataSet/grass/grass_");
+        tipo = 0;
     }
     else if (testeOuTreino == 0 && gramaOuAsfalto == 1){
         strcpy(nome,"teste_asphalt.txt");
         strcpy(diretorio,"DataSet/asphalt/asphalt_");
+        tipo = 1;
     }
     else if (testeOuTreino == 1 && gramaOuAsfalto == 0){
         strcpy(nome,"treino_grass.txt");
         strcpy(diretorio,"DataSet/grass/grass_");
+        tipo = 2;
     }
     else if (testeOuTreino == 1 && gramaOuAsfalto == 1){
         strcpy(nome,"treino_asphalt.txt");
         strcpy(diretorio,"DataSet/asphalt/asphalt_");
+        tipo = 3;
     }
     else{
         printf("ERRO");
     }
+    printf("Criando: %s \n", nome);
+
     FILE *arquivo = NULL;
     arquivo = fopen(nome, "wt");
     
@@ -90,10 +74,196 @@ FILE* salvaArquivos(int *array, int testeOuTreino, int gramaOuAsfalto){
          }else if(array[i]>=10){
             aux = sprintf(temp, "%s%d.txt\n", diretorio, array[i]);
             fprintf(arquivo, temp);
-            printf(temp);
         }
-    } 
+    }
+    fclose(arquivo); 
+    return tipo;
 }
+
+void nomeArquivo(int codigoArquivo, char* nome){
+    switch(codigoArquivo){        
+    case(0):
+    strcpy(nome,"teste_grass.txt");
+    break;
+    
+    case(1):
+    strcpy(nome,"teste_asphalt.txt");
+    break;
+    
+    case(2):
+    strcpy(nome,"treino_grass.txt");
+    break;
+    
+    case(3):
+    strcpy(nome,"treino_asphalt.txt");
+    break;
+    }
+}
+
+
+void pegaLinhaNome(int codigoArquivo, int linha, char* filename, char* conteudoLinha){
+    nomeArquivo(codigoArquivo, filename);
+    FILE *fptr;
+
+    fptr = fopen(filename, "r");
+    rewind(fptr);  
+    int count = 0;  
+    char buffer[256];
+    while (fgets(buffer, sizeof buffer, fptr)) 
+    {
+        if (count == linha){
+            break;
+        }
+        else{
+            count++;
+        }
+    }
+    
+    strtok(buffer, "\n");
+    printf("este eh o buffer %s\n", buffer);
+    strcpy(conteudoLinha, buffer);
+    fclose(fptr);
+}
+
+int* calculaDimensao(char* filename){
+    int *dimensaoMatriz = (int*) calloc (2,sizeof(int));
+
+    FILE *arq;
+    char c, end = '\n';
+    int eol = 0;
+    
+    dimensaoMatriz[1] = 1;
+
+    if ((arq = fopen(filename, "r")) == NULL) {
+        printf("Erro ao abrir o arquivo.");         
+    } 
+    else {
+        rewind(arq);
+        while(fread(&c, sizeof(char), 1, arq)) {
+            if (c == '\n'){
+                dimensaoMatriz[0]++;
+                eol = 1;
+            }
+            if(c == ';' && eol == 0){
+                dimensaoMatriz[1]++;
+            } 
+        }
+    }
+
+    fclose(arq);
+    return dimensaoMatriz;
+}
+
+void calculaILBP(int mat[], int lin, int col){
+    int i = 0;
+    int j = 0; 
+
+    // for (i = 0 ;i<lin;i++){
+    //     for(j=0;col;j++){
+          printf("%d\t", (*mat + 0)+0 ); 
+    //     }
+    //     printf("\n");
+    // }
+
+   int* ilbp = (int *)calloc(pow(2, 9), sizeof(int));
+   FILE* ilbp_file;
+   fopen("ilpb.txt", "w");
+    
+   int i, j;
+
+    for (i=0; i< lin; i++){
+        for(j=0; j < col; j++){
+        	
+        	int **aux = (int**)calloc(lin,sizeof(int*));
+		    int k;
+		    for(int k=0; k<lin; k++){
+		        aux[i]=(int*)calloc(col,sizeof(int));
+		    }
+
+	        // top line *(*(a+i)+j)
+	        *(*(aux+0)+0) = (i == 0 || j == 0) ? 0 : *(*(mat + ((i - 1) * col) + (j - 1)));
+	        *(*(aux+0)+1) = (i == 0) ? 0 : *(*(mat + ((i - 1) * col) + j));
+	        *(*(aux+0)+2) = (i == 0 || j == (col - 1)) ? 0 : *(*(mat + ((i - 1) * col) + (j + 1)));
+
+	        // center line
+	        *(*(aux+1)+0) = (j == 0) ? 0 : *(*(mat + (i * col) + (j - 1)));
+	        *(*(aux+1)+1) = *(*(mat + (i * col) + j)); // center piece
+	        *(*(aux+1)+2) = (j == (col - 1)) ? 0 : *(*(mat + (i * col) + (j + 1)));
+
+	        // botton line
+	        *(*(aux+2)+0) = (i == (lin - 1) || j == 0) ? 0 : *(*(mat + ((i + 1) * col) + (j - 1)));
+	        *(*(aux+2)+1) = (i == (lin - 1)) ? 0 : *(*(mat + ((i + 1) * col) + j));
+	        *(*(aux+2)+2) = (i == (lin - 1) || j == (col - 1)) ? 0 : *(*(mat + ((i + 1) * col) + (j + 1)));
+
+// 	        // aux[0][0] = (i == 0 || j == 0) ? 0 : img[i-1][j-1];
+// 	        // aux[0][1] = (i == 0) ? 0 : img[i-1][j];
+// 	        // aux[0][2] = (i == 0 || j == (col - 1)) ? 0 : img[i-1][j+1];
+
+// 	        // aux[1][0] = (j == 0) ? 0 : img[i][j-1];
+// 	        // aux[1][1] = img[i][j];
+// 	        // aux[1][2] = (j == (col - 1)) ? 0 : img[i][j+1];
+
+// 	        // aux[2][0] = (i == (lin - 1) || j == 0) ? 0 : img[i+1][j-1];
+// 	        // aux[2][1] = (i == (lin - 1)) ? 0 : img[i+1][j];
+// 	        // aux[2][2] = (i == (lin - 1) || j == (col - 1)) ? 0 : img[i+1][j+1];
+
+//             for (i=0; i<3; i++){
+//                 for (j=0;j<3;j++){
+//                     fprintf(ilbp_file, "%d", *(*(aux + i)+j));
+//                 }
+//                 printf("\n");
+//             }
+//                     printf("\n");
+//     	}
+// 	}
+ }
+
+
+void armazenaArquivoMatriz(char* filename){
+    int* dimensaoMatriz;
+    dimensaoMatriz = calculaDimensao(filename);
+    int lin = dimensaoMatriz[0];
+    int col = dimensaoMatriz[1];
+
+    printf("%d \t %s", dimensaoMatriz[0], lin);
+
+    FILE *file;
+    file = fopen(filename, "r");
+
+    int **matrizImagem = (int**)calloc(lin,sizeof(int*));
+
+    for(int i=0; i<lin; i++){
+        matrizImagem[i]=(int*)calloc(col,sizeof(int));
+    }
+
+    rewind(file);
+
+    if(file==NULL){
+    printf("ERRO!\n");
+    } else {
+        for(int i=0; i<lin-1; i++){
+          for(int j=0; j<col-1; j++){
+     	       fscanf(file, "%d%*c", &matrizImagem[i][j]);
+	        }
+        }
+    }
+	sleep(1);
+	//printf("Preparando imagem pro ILPB...");
+	sleep(1);
+//    calculaILBP(matrizImagem, lin, col);
+
+    printf("\n");
+
+    // for (int i=1;i<dimensaoMatriz[0]-1;i++){
+    //     for(int j=1;j<dimensaoMatriz[1]-1;j++){
+    //       printf("%d\t",matrizImagem[i][j] ); 
+    //     }
+    //     printf("\n");
+    // }
+
+    fclose(file);
+}
+
 
 int main () {
     int * ordemImagensAsfalto; 
@@ -110,55 +280,50 @@ int main () {
     
     int i;
 
+    printf("Sorteando arquivos de Treino... \n");
     for(i = 0; i < (25); ++i){
         asfaltoTreino[i] = *(ordemImagensAsfalto+i);
         gramaTreino[i] = *(ordemImagensGrama+i);
     }
+
+    sleep(1);
+
+    printf("Sorteando arquivos de Teste...\n");
     for(; i < 50; ++i){
         asfaltoTeste[i - 25] = *(ordemImagensAsfalto+i);
-        gramaTeste[i- 25] = *(ordemImagensGrama+i);
+        gramaTeste[i - 25] = *(ordemImagensGrama+i);
     }
+
+    sleep(1);
+    printf("Criando arquivos...\n");
     
-    FILE* listaArquivosTesteGrama = salvaArquivos(gramaTeste, 0, 0);
     sleep(1);
-    FILE* listaArquivosTesteAsfalto = salvaArquivos(asfaltoTeste, 0, 1);
+    int listaArquivosTesteGrama = salvaArquivos(gramaTeste, 0, 0); 
+ 
     sleep(1);
-    FILE* listaArquivosTreinoGrama = salvaArquivos(gramaTreino, 1, 0);
+    int listaArquivosTesteAsfalto = salvaArquivos(asfaltoTeste, 0, 1);
+ 
     sleep(1);
-    FILE* listaArquivosTreinoAsfalto = salvaArquivos(asfaltoTreino, 1, 1);
+    int listaArquivosTreinoGrama = salvaArquivos(gramaTreino, 1, 0);
+ 
+    sleep(1);
+    int listaArquivosTreinoAsfalto = salvaArquivos(asfaltoTreino, 1, 1);    
+    
+    char arquivoNome[50];
+    char conteudoLinha[256];
 
-   return 0;
+    int linha, codigoArquivo;
+    
+    //treinamento do sistema
+    
+    for (codigoArquivo = 2; codigoArquivo < 3; codigoArquivo++){ 
+        for (linha = 0; linha < 1; linha++){
+            sleep(1/60);
+            pegaLinhaNome(codigoArquivo, linha, arquivoNome, conteudoLinha);
+            armazenaArquivoMatriz(conteudoLinha);
+        }
+                    printf("-----------------\n");
+    }
+
+    return 0;
 }
-
-/* calculaDimensaoArquivo
-    params: 
-char* NomeArquivo = nome do arquivo
-FILE** arquivo = arquivo a ser aberto 
-    return: 
-int dimensaoMatriz = retorna um array com o número de linhas(dimensaoMatriz[0]) e colunas(dimensaoMatriz[1])
-*/
-
-// int* calculaDimensaoArquivo(char* nomeArquivo, FILE** arquivo){
-// }
-
-/* fazMatriz
-    params: 
-int*** matriz = matriz que vai ser passsada para ser alocada dinamicamente
-int* dimensaoMatriz = array com o número de linhas(dimensaoMatriz[0]) e colunas(dimensaoMatriz[1])
-    return: 
-matrizAlocada = matriz alocada com sucesso;
-return 0 = alocacao deu certo; 
-return 1 = alocacao falhou;
-*/
-//int fazMatriz(int*** matriz, int* dimensaoMatriz){}
-
-/* armazenaArquivo
-    params: 
-char* NomeArquivo = nome do arquivo
-FILE** arquivo = arquivo a ser aberto 
-    return: 
-arquivoMatriz = retorna arquivo armazenado numa matriz
-*/
-//int armazenaArquivo(char* nomeArquivo, FILE** arquivo){}
-
-
